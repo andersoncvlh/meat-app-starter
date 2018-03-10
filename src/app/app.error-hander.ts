9da31bc2bfdf59c8f40405e79e50dbfbfffa238b
anderson.cvlh <anderson.cvlh@gmail.com>
2018-03-10 15:29:11 -0300

    Operador catch e modelo inicial ErroHandler

---
+++ src/app/app.error-hander.ts
@@ -0,0 +1,20 @@
+import { Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/throw';
+
+export class ErrorHandler {
+
+  static handleError(error: Response | any) {
+    let errorMsg: string;
+
+    if (error instanceof Response) {
+      console.log(error);
+      errorMsg = `Erro ${error.status} ao acessar a URL ${error.url} - ${error.statusText}`;
+    } else {
+      errorMsg = error.toString();
+    }
+    console.log(errorMsg);
+    return Observable.throw(errorMsg);
+  }
+
+}
--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -2,9 +2,11 @@ import { MEAT_APP } from './../app.api';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 import { Restaurant } from './restaurant/restaurant.model';
 import { Http } from '@angular/http';
+import { ErrorHandler } from './../app.error-hander';
 
 @Injectable()
 export class RestaurantsService {
@@ -13,7 +15,8 @@ export class RestaurantsService {
 
   restaurants(): Observable<Restaurant[]> {
     return this.http.get(`${MEAT_APP}/restaurants`)
-      .map(response => response.json());
+      .map(response => response.json())
+      .catch(ErrorHandler.handleError);
   }
 
 }
